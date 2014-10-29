de9d8d58466a5539f5038789f4fda733717e99fe
JoeBiggia <joe.biggica@gmail.com>
2014-10-28 23:04:02 -0400

    profile page edit feature initialized

--- app/assets/javascripts/backbone/views/UserView.js
+++ app/assets/javascripts/backbone/views/UserView.js
@@ -10,13 +10,15 @@ MartialClub.Views.UserView = Backbone.View.extend({
 	tagName: 'li',
 	template: _.template( $('#user-list-template').html() ),
 	userPageTemplate: _.template( $('#user-page-template').html() ),
+	editUserTemplate: _.template( $('#edit-user-template').html() ),
 
 	events: {
 		"click i.edit-button" : "edit",
 	},
 
 	edit: function() {
-		console.log('edit')
+		this.$el.empty();
+		this.$el.html(this.editUserTemplate( { user: this.model.toJSON() }));
 		
 	},
 
