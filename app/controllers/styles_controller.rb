a3d40d968d27d9440e7592dd68e5c38d462f5049
JoeBiggia <joe.biggica@gmail.com>
2014-10-30 00:06:00 -0400

    final commit again

--- app/controllers/styles_controller.rb
+++ app/controllers/styles_controller.rb
@@ -1,7 +1,7 @@
 class StylesController < ApplicationController
 
 	def index
-		styles = Style.all.to_json(:include => :schools)
+		styles = Style.all.order(name: :asc).to_json(:include => :schools)
 		respond_to do |format|
     		format.json { render :json => styles }
 		end
